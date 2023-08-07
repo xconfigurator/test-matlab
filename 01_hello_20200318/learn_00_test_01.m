ce5d819f24af907c32eb472ebcabd883529e3810
liuyang <xconfigurator@hotmail.com>
2023-08-07 10:50:58 +0800

    增加视频出处笔记

--- 01_hello_20200318/learn_00_test_01.m
+++ 01_hello_20200318/learn_00_test_01.m
@@ -1,4 +1,6 @@
 % e.g. 判断三条边是否能组成三角形
+% https://www.bilibili.com/video/BV1Rx411S7CR/?spm_id_from=autoNext&vd_source=8bd7b24b38e3e12c558d839b352b32f4
+% 15:37
 a = input('Enter the value of a:');
 b = input('Enter the value of b:');
 c = input('Enter the value of c:');
--- 01_hello_20200318/learn_00_test_02.m
+++ 01_hello_20200318/learn_00_test_02.m
@@ -1,4 +1,6 @@
 % 一元二次方程求根
+% https://www.bilibili.com/video/BV1Rx411S7CR/?spm_id_from=autoNext&vd_source=8bd7b24b38e3e12c558d839b352b32f4
+% 20:22
 a = input('a = ');
 b = input('b = ');
 c = input('c = ');
